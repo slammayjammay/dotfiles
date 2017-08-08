ada24f81d40288914dae2566170c6e87f845594e
Scott McKenzie <scott.osmckenzie@proton.me>
2017-08-08 15:09:55 -0700

    Enable fallout theme by entering a command

--- hyper/hyper_plugins/local/fallout-theme/index.js
+++ hyper/hyper_plugins/local/fallout-theme/index.js
@@ -1,81 +1,75 @@
 // =============================================================================
-// Colors!
-// But disable for vim!
-// Opening vim will output(?) stuff to the terminal, which is then emitted by
-// Hyper. This is how we detect when vim opens and closes.
+// Fallout terminal theme.
+// 1) Sets the background image to a retro computer screen.
+// 2) Colors text as green.
 // =============================================================================
-
-const VIM_OPEN_OUTPUT = /\[\?2004h\[\?1049h\[\?1h\=\[\?2004h/;
-const VIM_CLOSE_OUTPUT = /\[\?25l\[\?2004l.*/;
-
-// for some reason, the terminal does not clear after exiting vim. This variable
-// is set in the plugin component, where execute the `clear` command in Hyper.
-let clearTerminal;
 
 exports.decorateConfig = (config) => {
 	return Object.assign({}, config, {
-		cursorColor: 'lightgreen',
+		css: `
+			${config.css || ''}
+
+			.fallout .hyper_main {
+				background-image: url(http://i.imgur.com/NFc1CXK.jpg) !important;
+			}
+			.fallout .hyper_main::before {
+				opacity: 0 !important;
+			}
+		`,
 		termCSS: `
 			${config.termCSS || ''}
 
-			/* stolen from hyper-krftwrk */
-			.fallout-theme x-row, .fallout-theme x-row span {
-				color: lightgreen;
-				text-shadow: 0 0 10px lightgreen;
+			.fallout x-row, .fallout x-row span {
+				color: lightgreen !important;
+				text-shadow: 0 0 10px lightgreen !important;
+			}
+
+			.fallout .cursor-node {
+				background-color: lightgreen !important;
+				border-color: lightgreen !important;
 			}
 		`
 	});
 }
-
-function openingVimEditor(data) {
-	return VIM_OPEN_OUTPUT.test(data);
-}
-
-function closingVimEditor(data) {
-	return VIM_CLOSE_OUTPUT.test(data);
-}
-
 exports.middleware = (store) => (next) => (action) => {
+	let shouldDoNextAction = true;
+
   if (action.type === 'SESSION_ADD_DATA') {
     const { data } = action;
 
-    if (openingVimEditor(data)) {
-      store.dispatch({
-				type: 'OPENING_VIM_EDITOR'
-			});
-    } else if (closingVimEditor(data)) {
+		// lol
+		if (/fallout!: command not found/.test(data)) {
 			store.dispatch({
-				type: 'CLOSING_VIM_EDITOR'
+				type: 'TOGGLE_FALLOUT'
 			});
-			next(action);
-			clearTerminal();
-    } else {
-			next(action);
+			shouldDoNextAction = false;
 		}
-  } else {
+	}
+
+	if (shouldDoNextAction) {
 		next(action);
 	}
 };
 
 exports.reduceUI = (state, action) => {
   switch (action.type) {
-    case 'OPENING_VIM_EDITOR':
-      return state.set('isVimEditor', true);
-		case 'CLOSING_VIM_EDITOR':
-			return state.set('isVimEditor', false);
+		case 'TOGGLE_FALLOUT':
+			return state.set('fallout', !state.fallout);
   }
   return state;
 };
 
 exports.mapTermsState = (state, map) => {
   return Object.assign(map, {
-    isVimEditor: state.ui.isVimEditor
+    isVimEditor: state.ui.isVimEditor,
+    fallout: state.ui.fallout
   });
 };
 
 const passProps = (uid, parentProps, props) => {
   return Object.assign(props, {
-    isVimEditor: parentProps.isVimEditor
+    isVimEditor: parentProps.isVimEditor,
+    fallout: parentProps.fallout
   });
 };
 
@@ -90,35 +84,37 @@ exports.decorateTerm = (Term, { React }) => {
 		}
 
 		_onTerminal(term) {
+			this.term = term;
+
       if (this.props.onTerminal) {
 				this.props.onTerminal(term);
 			}
 
       this._div = term.div_;
       this._window = term.document_.defaultView;
-			this._termBody = term.document_.body;
 
-			this._termBody.classList.add('fallout-theme');
-
-			clearTerminal = () => {
-				term.io.onVTKeystroke('clear\n');
-			};
+			this.mainBody = document.body;
+			this.termBody = term.document_.body;
     }
 
 		componentWillReceiveProps(nextProps) {
-			if (nextProps.isVimEditor && !this.props.isVimEditor) {
-				this.disable();
-      } else if (!nextProps.isVimEditor && this.props.isVimEditor) {
-				this.enable();
+			if (nextProps.fallout && !this.props.fallout) {
+				this.enableFallout();
+				this.term.onVTKeystroke('\n');
+      } else if (!nextProps.fallout && this.props.fallout) {
+				this.disableFallout();
+				this.term.onVTKeystroke('\n');
       }
     }
 
-		enable() {
-			this._termBody.classList.add('fallout-theme');
+		enableFallout() {
+			this.mainBody.classList.add('fallout');
+			this.termBody.classList.add('fallout');
 		}
 
-		disable() {
-			this._termBody.classList.remove('fallout-theme');
+		disableFallout() {
+			this.mainBody.classList.remove('fallout');
+			this.termBody.classList.remove('fallout');
 		}
 
 		render() {
