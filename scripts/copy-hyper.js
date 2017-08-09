94cd60d3769bdfafbd70891d8b926b61fc4ccc9f
slammayjammay <slammayjammay@gmail.com>
2017-08-09 16:02:03 -0700

    Split up scripts into separate files

---
+++ scripts/copy-hyper.js
@@ -0,0 +1,74 @@
+const { execSync } = require('child_process');
+const { join, basename } = require('path');
+const { readdirSync, statSync } = require('fs');
+const chalk = require('chalk');
+
+module.exports = (homeDirectory) => {
+	const SOURCE = join(homeDirectory, 'dotfiles', 'hyper/hyper_plugins/local');
+	const DESTINATION = join(homeDirectory, '.hyper_plugins/local');
+
+	function getPluginPaths() {
+		return readdirSync(SOURCE).map(file => join(SOURCE, file)).filter(file => {
+			return statSync(file).isDirectory(); // only directories
+		});
+	}
+
+	function installPlugin(path) {
+		console.log();
+		console.log(chalk.bold.green(`Installing plugin ${basename(path)}...`));
+
+		execSync(`cd ${path} && npm install && npm run build -- --hide-modules --progress`, { stdio: 'inherit' });
+		copyPlugin(path);
+
+		console.log();
+	}
+
+	function copyPlugin(path) {
+		process.stdout.write(chalk.bold(`Copying plugin ${basename(path)}...`));
+
+		execSync(`cp -r ${path} ${DESTINATION}`, { stdio: 'inherit' });
+		console.log('done.');
+	}
+
+	return {
+		installAll() {
+			console.log(chalk.bold('Hyper'));
+
+			const pluginPaths = getPluginPaths();
+			pluginPaths.forEach(path => {
+				installPlugin(path);
+			});
+		},
+
+		copyAll() {
+			const pluginPaths = getPluginPaths();
+			pluginPaths.forEach(path => {
+				copyPlugin(path);
+			});
+		},
+
+		install(pluginName) {
+			const pluginPath = getPluginPaths().find(path => {
+				return basename(path) === pluginName;
+			});
+
+			if (!pluginPath) {
+				throw new Error(`No plugin by the name of ${pluginName}`);
+			}
+
+			installPlugin(pluginPath);
+		},
+
+		copy(pluginName) {
+			const pluginPath = getPluginPaths().find(path => {
+				return basename(path) === pluginName;
+			});
+
+			if (!pluginPath) {
+				throw new Error(`No plugin by the name of ${pluginName}`);
+			}
+
+			installPlugin(pluginPath);
+		}
+	};
+};
---
+++ scripts/install.js
@@ -0,0 +1,18 @@
+const { spawnSync, execSync } = require('child_process');
+const { resolve, join } = require('path');
+const { existsSync, readdirSync, statSync } = require('fs');
+const chalk = require('chalk');
+
+const HOME_DIRECTORY = execSync('cd ~ && pwd').toString().trim();
+
+// sub-scripts
+const copyVim = require('./copy-vim');
+const copyHyper = require('./copy-hyper')(HOME_DIRECTORY);
+
+copyVim(HOME_DIRECTORY);
+console.log();
+
+copyHyper.installAll();
+console.log();
+
+console.log(chalk.bold.green('Installation successfull!'));
