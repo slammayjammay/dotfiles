2cc3aa76cf9e64187a99379fb928a8e1c01ea64e
Scott McKenzie <scott.osmckenzie@proton.me>
2017-08-08 17:10:58 -0700

    Add hyperterm-cursor plugin

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -118,7 +118,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hypernasa'],
+  plugins: ['hypernasa', 'hyperterm-cursor'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
