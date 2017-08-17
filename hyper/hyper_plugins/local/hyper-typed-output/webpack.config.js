b22b389d80f7d7f6b41a5e123928d810a2980dbf
Scott McKenzie <scott.osmckenzie@proton.me>
2017-08-17 11:47:51 -0700

    Move typed-ouput animation to its own hyper plugin

---
+++ hyper/hyper_plugins/local/hyper-typed-output/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	entry: `${__dirname}/index.js`,
+	output: {
+		path: `${__dirname}/dist`,
+		filename: 'index.js',
+		libraryTarget: 'commonjs'
+	},
+	target: 'node',
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				test: /.js$/,
+				query: {
+					presets: [
+						'es2015', 'react', 'stage-2'
+					].map(preset => require.resolve(`babel-preset-${preset}`))
+				}
+			}
+		]
+	}
+};
