38384f51dfd5244bbd8226027891f8f33e0f3df3
Scott McKenzie <scott.osmckenzie@proton.me>
2018-04-25 19:38:13 -0700

    Add plugin to set XTerm as transparent

---
+++ hyper/hyper_plugins/local/hyper-xterm-transparent/index.js
@@ -0,0 +1,21 @@
+exports.decorateTerm = (Term, { React }) => {
+	return class extends React.Component {
+		constructor(...args) {
+			super(...args);
+
+			this._onDecorated = this._onDecorated.bind(this);
+		}
+
+		_onDecorated(term) {
+			// set XTerm terminal as transparent. I think this is okay? the background
+			// color set in .hyper.js will still show through.
+			setTimeout(() => term.term.setOption('allowTransparency', true), 0);
+		}
+
+		render() {
+			return React.createElement(Term, Object.assign({}, this.props, {
+				onDecorated: this._onDecorated
+			}));
+		}
+	}
+};
