51d08c6c48ea0472781030e65153b7e079f4aae1
Scott McKenzie <scott.osmckenzie@proton.me>
2018-05-09 13:47:43 -0700

    Change postprocessing effects

--- hyper/hyper-postprocessing/index.js
+++ hyper/hyper-postprocessing/index.js
@@ -1,11 +1,6 @@
-const curvedMonitorShader = require('./curved-monitor-shader');
-const createRippleShader = require('./ripple-shader');
-const createGlitchShader = require('./glitch-shader');
+module.exports = ({ ShaderMaterial }) => {
+	const rippleShader = require('./ripple-shader')({ ShaderMaterial });
 
-module.exports = ({ ShaderPass, ShaderMaterial }) => {
-	const rippleShader = createRippleShader({ ShaderMaterial });
-	const glitchShader = createGlitchShader({ ShaderMaterial });
-
-	// return rippleShader;
-	// return [...glitchShader, curvedMonitorShader];
+	return [];
+	return [rippleShader];
 };
