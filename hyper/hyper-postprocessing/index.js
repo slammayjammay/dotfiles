b9e708da8bcbbc7cfaee8353680937e674077b4a
slammayjammay <slammayjammay@gmail.com>
2018-05-31 16:36:43 -0700

    Get film effect working

--- hyper/hyper-postprocessing/index.js
+++ hyper/hyper-postprocessing/index.js
@@ -1,4 +1,4 @@
-const postprocessingEffect = require('./effects/underwater');
+const postprocessingEffect = require('./effects/film');
 
 module.exports = ({ ShaderMaterial }) => {
 	return postprocessingEffect({ ShaderMaterial });
