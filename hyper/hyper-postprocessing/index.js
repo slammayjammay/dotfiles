a994d171f8f61155c6fc9d05f332d5b134a0ce5e
slammayjammay <slammayjammay@gmail.com>
2019-02-23 18:34:39 -0800

    Use bloom hyper effect

--- hyper/hyper-postprocessing/index.js
+++ hyper/hyper-postprocessing/index.js
@@ -1,8 +1,32 @@
-const { EffectPass } = require('postprocessing');
-const chalkEffects = require('./effects/chalk');
+const { resolve } = require('path');
+const { readFileSync } = require('fs');
+const { Uniform, LinearFilter, TextureLoader } = require('three');
+const { EffectPass, Effect, BloomEffect } = require('postprocessing');
 
-const effects = [
-	...chalkEffects
-];
+const PATH = '../hyper_plugins/local/hyper-postprocessing/examples/';
 
-module.exports = { pass: new EffectPass(null, ...effects) };
+const passes = [];
+
+passes.push(new EffectPass(null,
+	new Effect(
+		'scale',
+		readFileSync(resolve(__dirname, PATH, 'glsl/scale.glsl')).toString(),
+		{ defines: new Map([['scale', '0.95']]) }
+	),
+	new Effect(
+		'sampling',
+		readFileSync(resolve(__dirname, PATH, 'glsl/sampling.glsl')).toString(),
+		{ blendFunction: 12 }
+	)
+));
+
+passes.push(new EffectPass(null,
+	new BloomEffect({
+		kernelSize: 3,
+		distinction: -0.6
+	}))
+);
+
+module.exports = passes.map(pass => {
+	return { pass };
+});
