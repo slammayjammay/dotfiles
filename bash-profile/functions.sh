3790458c608cde54c902ed64af6668bd65dea0e0
slammayjammay <slammayjammay@gmail.com>
2020-03-17 16:46:11 -0700

    Update bash stuff

---
+++ bash-profile/functions.sh
@@ -0,0 +1,32 @@
+# Need to manually copy this for changes to take effect
+
+# Enter any custom functions here
+
+# Ex: alias `pipe-boy --help` as `pipeboyhelp`
+function help() {
+	pipe-boy --help
+}
+
+function git() {
+	/usr/local/bin/git -c color.ui=always $@
+}
+
+function gd() {
+	git diff $@
+}
+
+function gdm() {
+	git diff master $@
+}
+
+function gdmr() {
+	git diff master --name-only --relative
+}
+
+function gs() {
+	git status $@
+}
+
+function gsp() {
+	git status --porcelain
+}
