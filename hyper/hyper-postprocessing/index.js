4a02229a7b5d5b7d1b74d118a6498d0231381484
Scott McKenzie <slammayjammay@gmail.com>
2020-07-25 15:01:44 -0700

    Idk

--- hyper/hyper-postprocessing/index.js
+++ hyper/hyper-postprocessing/index.js
@@ -5,28 +5,34 @@ const { EffectPass, Effect, BloomEffect, BlendFunction } = require('postprocessi
 
 const PATH = '../hyper_plugins/local/hyper-postprocessing/examples/';
 
-const passes = [];
+const fn = require(resolve(__dirname, PATH, 'effects/retro'));
 
-passes.push(new EffectPass(null,
-	new Effect(
-		'scale',
-		readFileSync(resolve(__dirname, PATH, 'glsl/scale.glsl')).toString(),
-		{ defines: new Map([['scale', '0.95']]) }
-	),
-	new Effect(
-		'sampling',
-		readFileSync(resolve(__dirname, PATH, 'glsl/sampling.glsl')).toString(),
-		{ blendFunction: BlendFunction.NORMAL }
-	)
-));
+module.exports = function() {
+	return fn(...arguments);
+};
 
-passes.push(new EffectPass(null,
-	new BloomEffect({
-		kernelSize: 3,
-		distinction: -0.4
-	}))
-);
-
-module.exports = passes.map(pass => {
-	return { pass };
-});
+// const passes = [];
+//
+// passes.push(new EffectPass(null,
+// 	new Effect(
+// 		'scale',
+// 		readFileSync(resolve(__dirname, PATH, 'glsl/scale.glsl')).toString(),
+// 		{ defines: new Map([['scale', '0.95']]) }
+// 	),
+// 	new Effect(
+// 		'sampling',
+// 		readFileSync(resolve(__dirname, PATH, 'glsl/sampling.glsl')).toString(),
+// 		{ blendFunction: BlendFunction.NORMAL }
+// 	)
+// ));
+//
+// passes.push(new EffectPass(null,
+// 	new BloomEffect({
+// 		kernelSize: 3,
+// 		distinction: -0.4
+// 	}))
+// );
+//
+// module.exports = passes.map(pass => {
+// 	return { pass };
+// });
