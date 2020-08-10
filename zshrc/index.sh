b013caced605f292a8adc3d0bedba039ccc992fa
Scott McKenzie <slammayjammay@gmail.com>
2020-08-09 23:16:32 -0700

    Git completion

--- zshrc/index.sh
+++ zshrc/index.sh
@@ -2,6 +2,9 @@
 export VISUAL="vim"
 export EDITOR="sublime -w"
 
+fpath=(~/dotfiles/zshrc/completion $fpath)
+autoload -U compinit && compinit
+
 source "/Users/Scott/dotfiles/zshrc/prompt.sh"
 source "/Users/Scott/dotfiles/zshrc/aliases.sh"
 source "/Users/Scott/dotfiles/zshrc/nvm.sh"
--- zshrc/prompt.sh
+++ zshrc/prompt.sh
@@ -7,12 +7,12 @@ function git_prompt() {
 	branch=$(git rev-parse --abbrev-ref HEAD) &> /dev/null
 
 	if [[ $? -eq 0 ]]; then
-		echo " %F{green}($branch)%{$reset_color%}%F{red}$(evil_git_dirty)%{$reset_color%} "
+		echo " %{$fg[green]%}($branch)%{$reset_color%}%{$fg[red]%}$(evil_git_dirty)%{$reset_color%} "
 	else
 		echo " "
 	fi
 }
 
 setopt PROMPT_SUBST
-autoload colors && colors
+autoload -U colors && colors
 PROMPT='%1d %j$(git_prompt)\$ '
