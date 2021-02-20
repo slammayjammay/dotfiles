ecfa85f345f477866545676633f4f55fb4dd211e
Scott McKenzie <slammayjammay@gmail.com>
2021-02-20 11:40:17 -0800

    Learn about source better

--- zshrc/nvm.sh
+++ zshrc/nvm.sh
@@ -1,10 +1,10 @@
-export PATH="/Users/Scott/.nvm/versions/node/v12.18.2/bin:$PATH"
+export PATH="~/.nvm/versions/node/v12.18.2/bin:$PATH"
 
 is_nvm_loaded=false
 
 function nvm() {
 	if [ "$is_nvm_loaded" = false ]; then
-		. "/Users/Scott/.nvm/nvm.sh"
+		source ~/.nvm/nvm.sh
 	fi
 
 	nvm "$@"
