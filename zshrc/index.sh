59fc0c86b94be65dc7726304c28333ec91a1ad5f
Scott McKenzie <scott.osmckenzie@proton.me>
2021-02-23 23:42:59 -0800

    Fix I think

--- zshrc/index.sh
+++ zshrc/index.sh
@@ -8,5 +8,3 @@ autoload -U compinit && compinit
 source ~/dotfiles/zshrc/prompt.sh
 source ~/dotfiles/zshrc/aliases.sh
 source ~/dotfiles/zshrc/nvm.sh
-
-[[ $(uname) == "Linux" ]] && source ~/dotfiles/linux/index.sh
