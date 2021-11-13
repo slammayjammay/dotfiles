1dc0e9f06a817d104bc0709b1c8b0e2b95d899b0
Scott McKenzie <scott.osmckenzie@proton.me>
2021-11-13 13:38:51 -0800

    Delete old stuff

--- hyper/hyper-postprocessing/index.js
+++ hyper/hyper-postprocessing/index.js
@@ -1,38 +1,4 @@
 const { resolve } = require('path');
-const { readFileSync } = require('fs');
-const { Uniform, LinearFilter, TextureLoader } = require('three');
-const { EffectPass, Effect, BloomEffect, BrightnessContrastEffect, BlendFunction } = require('postprocessing');
-
 const PATH = '../hyper_plugins/local/hyper-postprocessing/examples/';
-
 const fn = require(resolve(__dirname, PATH, 'effects/retro'));
-
-module.exports = (...args) => {
-	return fn(...args);
-};
-
-// const passes = [];
-//
-// passes.push(new EffectPass(null,
-// 	new Effect(
-// 		'scale',
-// 		readFileSync(resolve(__dirname, PATH, 'glsl/scale.glsl')).toString(),
-// 		{ defines: new Map([['scale', '0.95']]) }
-// 	),
-// 	new Effect(
-// 		'sampling',
-// 		readFileSync(resolve(__dirname, PATH, 'glsl/sampling.glsl')).toString(),
-// 		{ blendFunction: BlendFunction.NORMAL }
-// 	)
-// ));
-//
-// passes.push(new EffectPass(null,
-// 	new BloomEffect({
-// 		kernelSize: 3,
-// 		distinction: -0.4
-// 	}))
-// );
-//
-// module.exports = passes.map(pass => {
-// 	return { pass };
-// });
+module.exports = (...args) => fn(...args);
