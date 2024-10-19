48fc9226d436a0bf7395df4e832e7cd3796d075f
Scott McKenzie <slammayjammay@gmail.com>
2024-10-19 13:03:08 -0700

    Delete hyper

--- hyper/hyper-postprocessing/index.js
+++
@@ -1,4 +0,0 @@
-const { resolve } = require('path');
-const PATH = '../hyper_plugins/local/hyper-postprocessing/examples/';
-const fn = require(resolve(__dirname, PATH, 'effects/retro'));
-module.exports = (...args) => fn(...args);
